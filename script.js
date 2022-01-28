7614953358403434c123e8085cdfc26b83722682
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2022-01-28 20:49:24 -0300

    Colocando novas imagens, botões e icones novos

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggle = document.querySelector('.toggle');
+const banner = document.querySelector('.banner');
+
+toggle.onclick = function() {
+  toggle.classList.toggle('active');
+  banner.classList.toggle('active');
+} 
